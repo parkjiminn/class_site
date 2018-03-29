5eedb353275c87bb3de5d183196caac5dc58c0c9
Jimin <jimingoose@gmail.com>
2018-03-28 20:03:28 -0400

    studio
    studio

---
+++ studio/container/007/scripts.js
@@ -0,0 +1,44 @@
+$(function(){
+
+//  $(".draggable").on("click",function(){
+
+//   $(".swing").hide();
+//   $(".ground").hide();
+//   $(".wall").hide();
+//   $(".draggable").show();
+// });
+
+
+ $(".swing").on("click",function(){
+
+  $(".wall").hide();
+  $(".ground").hide();
+  $(".draggable").hide();
+  $(".swing").show();
+ });
+
+ $(".ground").on("click",function(){
+
+  $(".swing").hide();
+  $(".wall").hide();
+  $(".draggable").hide();
+  $(".ground").show();
+ });
+
+ $(".wall").on("click",function(){
+
+  $(".swing").hide();
+  $(".ground").hide();
+  $(".draggable").hide();
+  $(".wall").show();
+ });
+
+ $( ".draggable" ).draggable();
+
+ // $(".hang").on("click",function(){
+
+ //  $(this).addClass("swing")
+ // });
+ 
+});
+
